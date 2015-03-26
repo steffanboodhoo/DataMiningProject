bee6530cc97f345016c64377a03c20c811b9c817
steffanboodhoo <boodhoo100@gmail.com>
2015-03-25 22:35:24 -0400

    dependencies

--- app/static/js/starter.js
+++ app/static/js/starter.js
@@ -1,11 +1,15 @@
 (function(window){
 	"use strict";
-	console.log("Main.js Succesfully loaded in browser");
+	console.log("starter.js Succesfully loaded in browser");
 
 	$(document).ready(function(){
 		//perform task when browser window is fully loaded
 		console.log('document ready')
+		createChart()
 	});
+	function createChart () {
+		var ctx = $("#myChart").get(0).getContext("2d");
+	}
 	
 }(this));
 	
