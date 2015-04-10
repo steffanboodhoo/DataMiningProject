64bac6d99bc109c205e2f3b81ad96a6b4f3dd753
jherezTaylor <jherez.taylor@gmail.com>
2015-04-10 07:40:55 -0400

    added panel for upload. ui not complete

---
+++ app/static/js/upload.js
@@ -0,0 +1,198 @@
+var inputType = "string";
+var stepped = 0, rowCount = 0, errorCount = 0, firstError;
+var start, end;
+var firstRun = true;
+var maxUnparseLength = 10000;
+
+$(function()
+{
+	// Tabs
+	$('#tab-string').click(function()
+	{
+		$('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('.input-area').hide();
+		$('#input-string').show();
+		$('#submit').text("Upload");
+		inputType = "string";
+	});
+
+	$('#tab-local').click(function()
+	{
+		$('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('.input-area').hide();
+		$('#input-local').show();
+		$('#submit').text("Upload");
+		inputType = "local";
+	});
+
+	$('#tab-remote').click(function()
+	{
+		$('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('.input-area').hide();
+		$('#input-remote').show();
+		$('#submit').text("Upload");
+		inputType = "remote";
+	});
+
+	// Upload invoked
+	$('#submit').click(function()
+	{
+		if ($(this).prop('disabled') == "true")
+			return;
+
+		stepped = 0;
+		rowCount = 0;
+		errorCount = 0;
+		firstError = undefined;
+
+		var config = buildConfig();
+		var input = $('#input').val();
+
+		if (inputType == "remote")
+			input = $('#url').val();
+
+		// Allow only one upload at a time
+		$(this).prop('disabled', true);
+
+		if (!firstRun)
+			console.log("--------------------------------------------------");
+		else
+			firstRun = false;
+
+
+
+		if (inputType == "local")
+		{
+			if (!$('#files')[0].files.length)
+			{
+				alert("Please select at least one file to upload.");
+				return enableButton();
+			}
+			
+			$('#files').parse({
+				config: config,
+				before: function(file, inputElem)
+				{
+					start = now();
+					console.log("Uploading file...", file);
+				},
+				error: function(err, file)
+				{
+					console.log("ERROR:", err, file);
+					firstError = firstError || err;
+					errorCount++;
+				},
+				complete: function()
+				{
+					end = now();
+					printStats("Done with all files");
+				}
+			});
+		}
+		else if (inputType == "remote" && !input)
+		{
+			alert("Please enter the URL of a file to download and parse.");
+			return enableButton();
+		}
+		else
+		{
+			start = now();
+			var results = Papa.parse(input, config);
+			console.log("Synchronous results:", results);
+			if (config.worker || config.download)
+				console.log("Running...");
+		}
+	});
+});
+
+function printStats(msg)
+{
+	if (msg)
+		console.log(msg);
+	console.log("       Time:", (end-start || "(Unknown; your browser does not support the Performance API)"), "ms");
+	console.log("  Row count:", rowCount);
+	if (stepped)
+		console.log("    Stepped:", stepped);
+	console.log("     Errors:", errorCount);
+	if (errorCount)
+		console.log("First error:", firstError);
+}
+
+
+
+function buildConfig()
+{
+	return {
+		delimiter: $('#delimiter').val(),
+		header: $('#header').prop('checked'),
+		dynamicTyping: $('#dynamicTyping').prop('checked'),
+		skipEmptyLines: $('#skipEmptyLines').prop('checked'),
+		preview: parseInt($('#preview').val() || 0),
+		step: $('#stream').prop('checked') ? stepFn : undefined,
+		encoding: $('#encoding').val(),
+		worker: $('#worker').prop('checked'),
+		comments: $('#comments').val(),
+		complete: completeFn,
+		error: errorFn,
+		download: inputType == "remote"
+	};
+}
+
+function stepFn(results, parser)
+{
+	stepped++;
+	if (results)
+	{
+		if (results.data)
+			rowCount += results.data.length;
+		if (results.errors)
+		{
+			errorCount += results.errors.length;
+			firstError = firstError || results.errors[0];
+		}
+	}
+}
+
+function completeFn(results)
+{
+	end = now();
+
+	if (results && results.errors)
+	{
+		if (results.errors)
+		{
+			errorCount = results.errors.length;
+			firstError = results.errors[0];
+		}
+		if (results.data && results.data.length > 0)
+			rowCount = results.data.length;
+	}
+
+	printStats("Parse complete");
+	console.log("    Results:", results);
+
+	// icky hack
+	setTimeout(enableButton, 100);
+}
+
+function errorFn(err, file)
+{
+	end = now();
+	console.log("ERROR:", err, file);
+	enableButton();
+}
+
+function enableButton()
+{
+	$('#submit').prop('disabled', false);
+}
+
+function now()
+{
+	return typeof window.performance !== 'undefined'
+			? window.performance.now()
+			: 0;
+}
