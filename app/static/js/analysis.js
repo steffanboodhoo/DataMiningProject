4ba6a5df812901af1ff992fa177b64ab3bca6653
steffanboodhoo <boodhoo100@gmail.com>
2015-04-11 15:47:45 -0400

    created wrapper and added theme

--- app/static/js/analysis.js
+++ app/static/js/analysis.js
@@ -1,31 +1,44 @@
 (function(window){
+	var containerCount = 0
 	var chartCount = 0
-
+	var technique = {'regr':'REGRESSION','clfy':'CLASSIFICATION'}
+	var charts = {'chartA':'CHARTA','chartB':'CHARTB'}
 	$(document).ready(function(){
 		console.log("im loaded")
+		setupTheme();
 		setupButtons();
 	});
 	function setupButtons(){
 		$('#addChart').click(function(){
-			createAnalysisView()
+			createAnalysisView(testData,testSeries,technique.regr)
 		})
 	}
-	function createAnalysisView(){
-		var divId = 'container'+chartCount
-		var chartId = 'chart'+chartCount
-		//container.attr('id',divId)
-		//container.attr('class','container-fluid')
-		var container = $("<div/>", {id: divId,class:'container-fluid'})
-		$("<div/>", {id: chartId,class:'container-fluid'}).appendTo(container)
-		$('#mid_pane').append(container)
-		createChartB(testData,testSeries,'#'+chartId)
-		//testChart('#'+chartId)
-		chartCount++
+
+	function createAnalysisView(data,labels,type){
+		var divId = 'container'+containerCount //create Unique container ID
+		var container = $("<div/>", {id: divId,class:'container-fluid'})//create container for this analysis
+		$('#mid_pane').append(container)//append it to the page
+		if(type===technique.regr){
+			//showing test data for regression
+			attachChart(data,labels,charts.chartB,divId)
+			//showing actual prediction for regression
+			attachChart(data,labels,charts.chartA,divId)
+		}else if(type === technique.clfy){
+			// attach what you want etc using attachChart
+		}
+		containerCount++;
 	}
 
-
-
-
+	function attachChart(data,labels,chartType,divId){
+		console.log('attach Chart'+chartType)
+		var chartId = 'chart'+chartCount //create unique ID for the container
+		$("<div/>", {id: chartId,class:'container-fluid'}).appendTo($('#'+divId)) //create the Container Itself and append it
+		if(chartType===charts.chartA)
+			createChartA(data,labels,'#'+chartId)
+		else if(chartType===charts.chartB)
+			createChartB(data, labels, '#'+chartId)
+		chartCount++
+	}
 
 
 	function createChartA (data,categories,chartContainer) {
@@ -162,61 +175,6 @@
 
 	}
 	
-	function testChart(container){
-		$(function () {
-		    $(container).highcharts({
-		        title: {
-		            text: 'Monthly Average Temperature',
-		            x: -20 //center
-		        },
-		        subtitle: {
-		            text: 'Source: WorldClimate.com',
-		            x: -20
-		        },
-		        xAxis: {
-		            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-		                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-		        },
-		        yAxis: {
-		            title: {
-		                text: 'Temperature (°C)'
-		            },
-		            plotLines: [{
-		                value: 0,
-		                width: 1,
-		                color: '#808080'
-		            }]
-		        },
-		        tooltip: {
-		            valueSuffix: '°C'
-		        },
-		        legend: {
-		            layout: 'vertical',
-		            align: 'right',
-		            verticalAlign: 'middle',
-		            borderWidth: 0
-		        },
-		        series: [{
-		            name: 'Tokyo',
-		            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-		        }, {
-		            name: 'New York',
-		            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-		        }, {
-		            name: 'Berlin',
-		            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-		        }, {
-		            name: 'London',
-		            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-		        }]
-		    });
-		});
-	}
-
-
-
-
-
 
 	var testData=[{
             name: 'Tokyo',
@@ -233,4 +191,220 @@
         }];
     var testSeries = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    //-------------------------------------------------------------------
+    function setupTheme(){
+    	/**
+		 * Dark theme for Highcharts JS
+		 * @author Torstein Honsi
+		 */
+
+		// Load the fonts
+		Highcharts.createElement('link', {
+		   href: '//fonts.googleapis.com/css?family=Unica+One',
+		   rel: 'stylesheet',
+		   type: 'text/css'
+		}, null, document.getElementsByTagName('head')[0]);
+
+		Highcharts.theme = {
+		   colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+		      "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
+		   chart: {
+		      backgroundColor: {
+		         linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+		         stops: [
+		            [0, '#2a2a2b'],
+		            [1, '#3e3e40']
+		         ]
+		      },
+		      style: {
+		         fontFamily: "'Unica One', sans-serif"
+		      },
+		      plotBorderColor: '#606063'
+		   },
+		   title: {
+		      style: {
+		         color: '#E0E0E3',
+		         textTransform: 'uppercase',
+		         fontSize: '20px'
+		      }
+		   },
+		   subtitle: {
+		      style: {
+		         color: '#E0E0E3',
+		         textTransform: 'uppercase'
+		      }
+		   },
+		   xAxis: {
+		      gridLineColor: '#707073',
+		      labels: {
+		         style: {
+		            color: '#E0E0E3'
+		         }
+		      },
+		      lineColor: '#707073',
+		      minorGridLineColor: '#505053',
+		      tickColor: '#707073',
+		      title: {
+		         style: {
+		            color: '#A0A0A3'
+
+		         }
+		      }
+		   },
+		   yAxis: {
+		      gridLineColor: '#707073',
+		      labels: {
+		         style: {
+		            color: '#E0E0E3'
+		         }
+		      },
+		      lineColor: '#707073',
+		      minorGridLineColor: '#505053',
+		      tickColor: '#707073',
+		      tickWidth: 1,
+		      title: {
+		         style: {
+		            color: '#A0A0A3'
+		         }
+		      }
+		   },
+		   tooltip: {
+		      backgroundColor: 'rgba(0, 0, 0, 0.85)',
+		      style: {
+		         color: '#F0F0F0'
+		      }
+		   },
+		   plotOptions: {
+		      series: {
+		         dataLabels: {
+		            color: '#B0B0B3'
+		         },
+		         marker: {
+		            lineColor: '#333'
+		         }
+		      },
+		      boxplot: {
+		         fillColor: '#505053'
+		      },
+		      candlestick: {
+		         lineColor: 'white'
+		      },
+		      errorbar: {
+		         color: 'white'
+		      }
+		   },
+		   legend: {
+		      itemStyle: {
+		         color: '#E0E0E3'
+		      },
+		      itemHoverStyle: {
+		         color: '#FFF'
+		      },
+		      itemHiddenStyle: {
+		         color: '#606063'
+		      }
+		   },
+		   credits: {
+		      style: {
+		         color: '#666'
+		      }
+		   },
+		   labels: {
+		      style: {
+		         color: '#707073'
+		      }
+		   },
+
+		   drilldown: {
+		      activeAxisLabelStyle: {
+		         color: '#F0F0F3'
+		      },
+		      activeDataLabelStyle: {
+		         color: '#F0F0F3'
+		      }
+		   },
+
+		   navigation: {
+		      buttonOptions: {
+		         symbolStroke: '#DDDDDD',
+		         theme: {
+		            fill: '#505053'
+		         }
+		      }
+		   },
+
+		   // scroll charts
+		   rangeSelector: {
+		      buttonTheme: {
+		         fill: '#505053',
+		         stroke: '#000000',
+		         style: {
+		            color: '#CCC'
+		         },
+		         states: {
+		            hover: {
+		               fill: '#707073',
+		               stroke: '#000000',
+		               style: {
+		                  color: 'white'
+		               }
+		            },
+		            select: {
+		               fill: '#000003',
+		               stroke: '#000000',
+		               style: {
+		                  color: 'white'
+		               }
+		            }
+		         }
+		      },
+		      inputBoxBorderColor: '#505053',
+		      inputStyle: {
+		         backgroundColor: '#333',
+		         color: 'silver'
+		      },
+		      labelStyle: {
+		         color: 'silver'
+		      }
+		   },
+
+		   navigator: {
+		      handles: {
+		         backgroundColor: '#666',
+		         borderColor: '#AAA'
+		      },
+		      outlineColor: '#CCC',
+		      maskFill: 'rgba(255,255,255,0.1)',
+		      series: {
+		         color: '#7798BF',
+		         lineColor: '#A6C7ED'
+		      },
+		      xAxis: {
+		         gridLineColor: '#505053'
+		      }
+		   },
+
+		   scrollbar: {
+		      barBackgroundColor: '#808083',
+		      barBorderColor: '#808083',
+		      buttonArrowColor: '#CCC',
+		      buttonBackgroundColor: '#606063',
+		      buttonBorderColor: '#606063',
+		      rifleColor: '#FFF',
+		      trackBackgroundColor: '#404043',
+		      trackBorderColor: '#404043'
+		   },
+
+		   // special colors for some of the
+		   legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+		   background2: '#505053',
+		   dataLabelsColor: '#B0B0B3',
+		   textColor: '#C0C0C0',
+		   contrastTextColor: '#F0F0F3',
+		   maskColor: 'rgba(255,255,255,0.3)'
+		};
+		// Apply the theme
+		Highcharts.setOptions(Highcharts.theme);
+    }
 }(this));
