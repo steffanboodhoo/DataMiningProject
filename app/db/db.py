b60093a428ebb6e3567eda4f2568934507aafe01
steffanboodhoo <boodhoo100@gmail.com>
2015-04-14 21:06:23 -0400

    database functions

--- app/db/db.py
+++ app/db/db.py
@@ -3,14 +3,28 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dataset_database
 
-def insertTest():
-	dataset = db.dataset
-	dataset.insert({'setA':[['a','b','c'],['a','b']]})
+def createDataset(dataObj):
+	if dataObj['purpose']=='training':
+		dataset = db.Traindataset
+	elif dataObj['purpose']=='mining':
+		dataset = db.Minedataset
+	dataset.insert(dataObj)
 
-def getTest():
-	dataset = db.dataset
-	for data in dataset.find():
-		print data 
+def getTrainDataset(name):
+	dataset = db.Traindataset
+	resp = dataset.find_one({'name':name})
+	return dumps(resp)
+
+def getMineDataset(name):
+	dataset = db.Minedataset
+	resp = dataset.find_one({'name':name})
+	return dumps(resp)
+
+def getAllTrain(dataset_type):
+	dataset = db.Traindataset
+	resp = []
+	for d in dataset.find({'type':dataset_type},{'name':1,'type':1}):
+		resp.append()
 
 if __name__ == '__main__':
 	#insertTest()
